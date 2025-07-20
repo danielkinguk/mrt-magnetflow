7af0dcf0864d4e37d9651773f5a3c9269aa0b521
Daniel King <dk@danielking.net>
2025-07-20 20:47:24 +0000

    Can you add custom logo (attached) to top tight. It should be displayed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { SidebarTrigger } from "./ui/sidebar";
 import { ThemeToggle } from "./theme-toggle";
+import Image from 'next/image';
 
 export function Header() {
   return (
@@ -8,7 +9,10 @@ export function Header() {
         <div className="flex items-center gap-2">
             <SidebarTrigger />
         </div>
-        <div className="flex flex-1 items-center justify-end space-x-2">
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <div className="p-1 border border-gray-200 rounded-md">
+            <Image src="/images/logo.png" alt="DK Apps Logo" width={100} height={36} />
+          </div>
           <ThemeToggle />
         </div>
       </div>
